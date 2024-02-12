0e2a1e0b05dbfcdbe32f31ae8a64e1dc45039bc6
savocks <giulio.savoca97@gmail.com>
2024-02-12 18:19:54 +0100

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
-import process from 'process';
 export const environment = {
-  supabaseUrl: process.env['SUPABASE_URL'],
-  supabaseKey: process.env['SUPABASE_KEY']
+  supabaseUrl: 'https://jdxhmpdmuqjaytyyzmmy.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkeGhtcGRtdXFqYXl0eXl6bW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3NTUzMTcsImV4cCI6MjAyMzMzMTMxN30.aWUD9wWMq1dydMjdOg-5Yc8R9QrpHEvEkaKL69gPX70'
 };
